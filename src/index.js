b6c23a96e471dddcd755f8c9d952c6c8cf7ec642
jannet18 <janetngei18@gmail.com>
2024-05-23 23:01:42 +0300

    server setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import userRoutes from "./routes/Users.js";
+import authRoutes from "./routes/auth.js";
+
+await mongoose.connect(process.env.MONGODB_CONNECTION);
+
+const app = express();
+app.use(express.json()); //converts body of API into json automatically
+app.use(express.urlencoded({ extended: true })); // parse the url and get url parameters
+app.use(cors());
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+  console.log("Server is active");
+});
