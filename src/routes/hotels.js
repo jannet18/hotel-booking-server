e82227bb191b868f43b1009305f1955a187f5a80
jannet18 <janetngei18@gmail.com>
2024-06-27 16:17:26 +0300

    added hotel detail route

--- src/routes/hotels.js
+++ src/routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
-
+import { param, validationResult } from "express-validator";
 const router = express.Router();
 
 router.get("/search", async (req, res) => {
@@ -49,57 +49,23 @@ router.get("/search", async (req, res) => {
   }
 });
 
-// router.get("/search", async (req, res) => {
-//   try {
-//     const {
-//       destination,
-//       checkIn,
-//       checkOut,
-//       adultCount,
-//       childCount,
-//       page = 1,
-//       limit = 5,
-//       sort = "name",
-//       order = "asc",
-//       minPrice,
-//       maxPrice,
-//       facilities = [],
-//     } = req.query;
-
-//     const parsedPage = parseInt(page, 10);
-//     const parsedLimit = parseInt(limit, 10);
-//     const skip = (parsedPage - 1) * parsedLimit;
-
-//     const sortOptions = {};
-//     sortOptions[sort] = order === "asc" ? 1 : -1;
-
-//     const query = {
-//       destination: new RegExp(destination, "i"),
-//       checkIn: { $gte: new Date(checkIn) },
-//       checkOut: { $lte: new Date(checkOut) },
-//       adultCount: { $gte: parseInt(adultCount, 10) },
-//       childCount: { $gte: parseInt(childCount, 10) },
-//       pricePerNight: {
-//         ...(minPrice && { $gte: parseFloat(minPrice) }),
-//         ...(maxPrice && { $gte: parseFloat(maxPrice) }),
-//       },
-//       facilities: { $all: facilities },
-//     };
-
-//     const hotels = await Hotel.find(query)
-//       .sort(sortOptions)
-//       .skip(skip)
-//       .limit(parsedLimit);
-
-//     const totalResults = await Hotel.countDocuments(query);
-
-//     res
-//       .status(200)
-//       .json({ hotels, totalResults, page: parsedPage, limit: parsedLimit });
-//   } catch (error) {
-//     res.status(500).json({ message: "Erro fetching hotels", error });
-//   }
-// });
+router.get(
+  "/:id",
+  [param("id").notEmpty().withMessage("Hotel ID is required")],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const id = req.params.id.toString();
+    try {
+      const hotel = await Hotel.findById(id);
+      res.json(hotel);
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching hotel!" });
+    }
+  }
+);
 const constructSearchQuery = (queryParams) => {
   let constructedQuery = {};
 
