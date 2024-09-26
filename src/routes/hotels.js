e962ec6c4600c4e37ea6bbbd7f647c499e48603e
jannet18 <janetngei18@gmail.com>
2024-09-26 15:19:00 +0300

    hotels api

--- src/routes/hotels.js
+++ src/routes/hotels.js
@@ -54,6 +54,15 @@ router.get("/search", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotel.find().sort("-lastUpdated");
+    res.json(hotels);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
 router.get(
   "/:id",
   [param("id").notEmpty().withMessage("Hotel ID is required")],
